d5b3990b6aa0758c549cb9fcbb8f9c23b61d7ab5
pooja.sh <pooja10838@gmail.com>
2020-04-23 20:40:33 +0530

    modified html content for gender dist

--- pages/overview.py
+++ pages/overview.py
@@ -114,14 +114,13 @@ def create_layout(app):
                     (
                         [
                                 html.Div(
-                                            [ html.H6(["Gender Distribution"], className="subtitle padded"),
+                                            [html.H6(["Gender Distribution"], className="subtitle padded"),
                                                 dcc.Graph(id='gender_pie',
                                                           figure={'data': [go.Pie(labels=genSumm['gender'],values=genSumm['nusers'])
                                                                           ],
                                                                    'layout' : layout},
-                                                         )
+                                                         ),
                                             ],
-
                                 ),
                         ],
                         className="sub-page", id="sub-page"
