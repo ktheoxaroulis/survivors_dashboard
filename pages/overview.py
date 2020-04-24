bce72311bbeef0776c9b7a6a4188474644f7f84e
pooja.sh <pooja10838@gmail.com>
2020-04-24 19:30:36 +0530

    added map

--- pages/overview.py
+++ pages/overview.py
@@ -180,23 +180,23 @@ def create_layout(app):
                         className="row",
                         style={"margin-bottom": "35px"},
                     ),
-                    # # WOrld Map
-                    # html.Div
-                    #     (
-                    #     [
-                    #         html.Div(
-                    #             [html.H6(["Number of cases by country"], className="subtitle padded"),
-                    #              dcc.Graph(id='map_country',
-                    #                        figure=fig_map,
-                    #                        ),
-                    #              ],
-                    #             style={"height": "1%", "width": "50%"},
-                    #         ),
-                    #     ],
-                    #     # className="sub-page", id="sub-page"
-                    #     className="row",
-                    #     style={"margin-bottom": "35px"},
-                    # ),
+                    # WOrld Map
+                    html.Div
+                        (
+                        [
+                            html.Div(
+                                [html.H6(["Number of cases by country"], className="subtitle padded"),
+                                 dcc.Graph(id='map_country',
+                                           figure=fig_map,
+                                           ),
+                                 ],
+                                style={"height": "3%", "width": "50%"},
+                            ),
+                        ],
+                        # className="sub-page", id="sub-page"
+                        className="row",
+                        style={"margin-bottom": "35px"},
+                    ),
 
                 ]
             ),
